151fcc68719e765c738f527c2d991ab50b6006ed
Nick Phillips <nick.phillips@3plearning.com>
2025-10-15 23:33:36 +1100

    bkhgiyg976r5re4e65etfgvjhfdstrdf

--- src/3PL/query/0A21DACB-147B-4B4A-B1E1-B40062D3503D.query-meta.sql
+++ src/3PL/query/0A21DACB-147B-4B4A-B1E1-B40062D3503D.query-meta.sql
@@ -1,10 +1,10 @@
 SELECT
-	lead.Id as LeadKey,
-    LEFT(lead.FirstName,40) AS FirstName,
-    LEFT(lead.LastName,80) AS LastName,
+    lead.Id as LeadKey,
+    LEFT (lead.FirstName, 40) AS FirstName,
+    LEFT (lead.LastName, 80) AS LastName,
     lead.Name as FullName,
-    LEFT(lead.Email,254) AS Email,
-    LEFT(lead.MobilePhone,50) AS MobilePhone,
+    LEFT (lead.Email, 254) AS Email,
+    LEFT (lead.MobilePhone, 50) AS MobilePhone,
     lead.Locale__c as Locale,
     lead.Status,
     lead.Lead_Stage__c as Stage,
@@ -18,8 +18,8 @@ SELECT
     lead.Marketing_Interest__c as MarketingInterest,
     lead.Marketing_Product_Interest__c as MarketingProductInterest,
     lead.Product_Interest__c as ProductInterest,
-    LEFT(lead.Title,126) AS JobTitle,
-    LEFT(lead.Job_Function__c,266) AS LeadJobFunction,
+    LEFT (lead.Title, 126) AS JobTitle,
+    LEFT (lead.Job_Function__c, 266) AS LeadJobFunction,
     lead.Contact_Role__c as ContactRole,
     lead.Existing_Customer__c as IsExistingCustomer,
     lead.Company,
@@ -32,24 +32,50 @@ SELECT
     lead.Territory__c as Territory,
     lead.Local_Authority__c AS LocalAuthority,
     lead.Clever__c as Clever,
-    LEFT(own.Email,128) AS OwnerEmail,
-    LEFT(own.FirstName,40) AS OwnerFirstName,
-    LEFT(own.LastName,80) AS OwnerLastName,
+    LEFT (own.Email, 128) AS OwnerEmail,
+    LEFT (own.FirstName, 40) AS OwnerFirstName,
+    LEFT (own.LastName, 80) AS OwnerLastName,
     lead.Campaign_Name__c AS CampaignName,
     lead.Campaign_Code__c AS CampaignCode,
     lead.CurrencyIsoCode AS LeadCurrencyIsoCode,
     lead.EmailBouncedDate
 FROM
-    ENT.Lead_Salesforce lead LEFT JOIN  
-    ENT.User_Salesforce own  ON lead.OwnerId = own.Id 
+    ENT.Lead_Salesforce lead
+    LEFT JOIN ENT.User_Salesforce own ON lead.OwnerId = own.Id
 WHERE
-	lead.Status IN ('Closed - Lost','Uncontactable – Nurture','Unqualified') AND	
-	lead.OwnerId != '00G2y000000YaqF' AND                     /*Parent Lead Queue ID*/
-	lead.Product_Interest__c LIKE '%Brightpath Progress Writing%' AND
-(lead.Reason_Lost__c NOT IN ( 'Fake','Duplicate','Parent Enquiry','Already Subscribed','Already subscribed but unaware','Home/Parent User','Combination - MX & MS' )
-	 or (lead.Reason_Lost__c ='' or lead.Reason_Lost__c is null)) AND
-	lead.IsConverted = 0 AND
-	lead.HasOptedOutOfEmail = 0 AND
-	lead.Email is NOT NULL AND
-	lead.CountryCode IN ('AU','NZ' ) AND
-	((lead.Title NOT LIKE '%Parent%' AND lead.Title NOT LIKE '%Student%') or (lead.Title ='' or lead.Title is null))
+    lead.Status IN (
+        'Closed - Lost',
+        'Uncontactable – Nurture',
+        'Unqualified'
+    )
+    AND lead.OwnerId != '00G2y000000YaqF'
+    AND /*Parent Lead Queue ID*/ lead.Product_Interest__c LIKE '%Brightpath Progress Writing%'
+    AND (
+        lead.Reason_Lost__c NOT IN (
+            'Fake',
+            'Duplicate',
+            'Parent Enquiry',
+            'Already Subscribed',
+            'Already subscribed but unaware',
+            'Home/Parent User',
+            'Combination - MX & MS'
+        )
+        OR (
+            lead.Reason_Lost__c = ''
+            OR lead.Reason_Lost__c is null
+        )
+    )
+    AND lead.IsConverted = 0
+    AND lead.HasOptedOutOfEmail = 0
+    AND lead.Email is NOT NULL
+    AND lead.CountryCode IN ('AU', 'NZ')
+    AND (
+        (
+            lead.Title NOT LIKE '%Parent%'
+            AND lead.Title NOT LIKE '%Student%'
+        )
+        OR (
+            lead.Title = ''
+            OR lead.Title is null
+        )
+    )
