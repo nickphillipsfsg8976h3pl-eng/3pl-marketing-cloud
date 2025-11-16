8e73758dbc53fdd38c1f8ec69ea4b94367e2fd95
Nick Phillips <Nick Phillips>
2025-11-16 10:32:15 +1000

    ad67a9876fa98s7df

---
+++ TEMP/scratchpad.php
@@ -0,0 +1,13 @@
+<script runat="server">
+    Platform.Load("core", "1");
+
+    try {
+
+
+        //...code here....
+
+
+    } catch (error) {
+        Write(Stringify(error.message));
+    }
+</script>
