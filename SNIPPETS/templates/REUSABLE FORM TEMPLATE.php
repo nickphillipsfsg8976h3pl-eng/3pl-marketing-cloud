d1b1d9f743cb4a9fa57df22c814ca07d34ec89a9
Nick Phillips <Nick Phillips>
2025-11-17 08:20:34 +1000

    a76f89a6f8976f897as6f8fasdfasdfadfs

--- SNIPPETS/templates/REUSABLE FORM TEMPLATE.php
+++ SNIPPETS/templates/REUSABLE FORM TEMPLATE.php
@@ -160,8 +160,8 @@
 
 
     //push to queue
-    var pipeline = DataExtension.Init("FORM_SUBMISSION_QUEUE");
-    pipeline.Rows.Add({
+    var queue = DataExtension.Init("REUSABLE_FORM_SUBMISSION_QUEUE");
+    queue.Rows.Add({
       "submission_id": Platform.Function.GUID(),
       "submission_name": payload.first_name + ' ' + payload.last_name,
       "submission_email": payload.email_address,
@@ -199,82 +199,9 @@
     Write("<br><br>");
 
 
-
-    //=====================================================================================================
-    //========================================== PIPELINE =================================================
-    //=====================================================================================================
-
-
-
-    /************************* 
-    --------- VALIDATE --------
-    **************************/
-
-
-    //profanity
-    //valid email
-    //known email
-    //student/parents
-
-
-    /************************* 
-    --------- PROCESS --------
-    **************************/
-
-
-    // Lookup Region, Country
-    if (payload.country_code) {
-      payload.lookupCountryData = Platform.Function.LookupRows('COUNTRY_REFERENCE', 'CountryCode', payload.country_code);
-      payload.region = payload.lookupCountryData[0].Region;
-      payload.country_name = payload.lookupCountryData[0].CountryName;
-    }
-
-    // Lookup State
-    if (payload.state_code) {
-      payload.lookupStateData = Platform.Function.LookupRows('STATE_REFERENCE', 'CountryCode', payload.country_code);
-      payload.state_name = payload.lookupStateData[0].StateName;
-    }
-
-    // Lookup Job Function
-    if (payload.job_title) {
-      paylaod.lookupJobData = Platform.Function.LookupRows('JOB_FUNCTION_REFERENCE', ['JobTitle', 'Region'], [payload.job_title, payload.region]);
-      payload.job_function = payload.lookupJobData[0].JobFunction;
-    }
-
-    // Lookup Enquiry Type
-    if (payload.eid) {
-      paylaod.lookupEnquiryData = Platform.Function.LookupRows('ENQUIRY_REFERENCE', ['Id'], [payload.eid]);
-      payload.enquiry = payload.lookupEnquiryData[0].Enquiry;
-      payload.description = payload.lookupEnquiryData[0].Description;
-      payload.enquiry_summary = payload.lookupEnquiryData[0].Description;
-    }
-
-    // Lookup Lead Status
-    if (payload.sid) {
-      paylaod.lookupLeadStatusData = Platform.Function.LookupRows('LEAD_STATUS_REFERENCE', ['Id'], [payload.sid]);
-      payload.status = payload.lookupLeadStatusData[0].Status;
-    }
-
-    // Lookup Campaign
-    if (payload.cid) {
-      paylaod.lookupCampaignData = Platform.Function.LookupRows('ENT.Campaign_Salesforce', 'Id', payload.cid);
-      payload.campaign_name = payload.lookupCampaignData[0].Name;
-    }
-
-    // Lookup Campaign Resources
-    //
-    //  TODO - configure to pull from an object related to the campaign instead of having to maintain a reference DE
-    //
-
-
-    /************************* 
-    ---------- UPSERT --------
-    **************************/
-
-
-    // Upsert Lead
-    // Upsert Campaign Member
-
+    /*******************************
+    ------------ ERROR -------------
+    ********************************/
 
 
   } catch (error) {
@@ -342,7 +269,7 @@
 
 
     /*******************************
-     --------- PRE-PROCESS ---------
+     ------ ADDITIONAL VALUES ------
     ********************************/
 
 
@@ -466,6 +393,12 @@
       Write('<br><br>')
       Write('Configuration: ' + Stringify(config));
     }
+
+
+
+    /*******************************
+    ------------ ERROR -------------
+    ********************************/
 
 
   } catch (error) {
