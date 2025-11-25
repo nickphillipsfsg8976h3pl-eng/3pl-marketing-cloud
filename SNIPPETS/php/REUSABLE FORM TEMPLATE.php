83c9af1c8165f9b4213c95aeff3e58ebd768af3d
Nick Phillips <Nick Phillips>
2025-11-26 08:10:59 +1000

    f6a867f89asd6fa9d8f69a8s6f97s8df6a898fd

--- SNIPPETS/php/REUSABLE FORM TEMPLATE.php
+++ SNIPPETS/php/REUSABLE FORM TEMPLATE.php
@@ -534,6 +534,24 @@ overidde all displayed inputs and update here and pipeline
             min-width: 0 !important;
         }
 
+        .preloader-container {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100vh;
+            background-color: rgba(255, 255, 255, 0.5);
+            z-index: 9999;
+        }
+
+        .preloader-wrapper {
+            position: absolute;
+            bottom: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            animation: none !important;
+        }
+
         [data-custom-message] {
             display: inline-block;
             font-size: 12px;
@@ -559,6 +577,25 @@ overidde all displayed inputs and update here and pipeline
 
 
 <body>
+
+
+    <!------------- Preloader ----------------->
+
+    <div data-preloader class="preloader-container hide">
+        <div class="preloader-wrapper big active">
+            <div class="spinner-layer spinner-green-only">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <!------------- FORM ----------------->
@@ -2891,8 +2928,8 @@ overidde all displayed inputs and update here and pipeline
                 if (isFormValidToSubmit) {
 
                     //show loader
-                    const loader = document.createElement('div');
-                    loader.classList.add('loader');
+                    const preloaderElement = document.querySelector('[data-preloader]');
+                    preloaderElement.classList.remove('hide');
 
                     //submit form
                     form.submit();
